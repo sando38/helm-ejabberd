2e1be840ec6822ff939dc75ce2ae980303b77d4a
sando38 <sandomir@tutanota.com>
2023-08-28 09:17:05 +0200

    healthcheck: if pod is detected as unhealthy, mark it as unhealthy

--- image/78f81de252dc932cd47b91d1a84ca8e8f0647498/scripts/healthcheck.sh
+++ image/78f81de252dc932cd47b91d1a84ca8e8f0647498/scripts/healthcheck.sh
@@ -6,8 +6,13 @@ headless_svc="$(hostname -d)" # e.g. servicename.namespace.default.svc.cluster.l
 sts_name="$(echo $pod_name | sed 's|-[0-9]\+||g')"
 api_url="127.0.0.1:5281"
 pod_status="$(wget -O - --post-data '{}' $api_url/api/status || echo 'unhealthy')"
-cluster_member_count="$(wget -q -O - --post-data '{}' $api_url/api/list_cluster | egrep -o '[^][]+' | tr ',' '\n' | wc -l)"
 election_leader="$(wget -cq ${ELECTION_URL:-127.0.0.1:4040} -O - | jq -r .leader)"
+
+# In some cases it needs to be called twice.
+_cluster_member_count() {
+    count="$(wget -q -O - --post-data '{}' $api_url/api/list_cluster | egrep -o '[^][]+' | tr ',' '\n' | wc -l)"
+    printf "$count"
+}
 
 if [ ! "$pod_status" = 'unhealthy' ] && [ -e $HOME/.ejabberd_ready ]
 then
@@ -18,12 +23,19 @@ then
             ## TODO: Consider 'set_master self' here to counter brain-splits.
             # ejabberdctl set_master self && return 0
             return 0
-        elif [ "$cluster_member_count" = "1" ]
+        elif [ "$(_cluster_member_count)" = "1" ]
         then
+            rm $HOME/.ejabberd_ready
             ## try re-joining ejabberd cluster
             join_pod_name="$election_leader.${headless_svc}"
-            ejabberdctl join_cluster "$sts_name@$join_pod_name" && sleep 5 && return 0
-        elif [ ! "$election_leader" = "$pod_name" ] && [ "$cluster_member_count" -gt "1" ]
+            ejabberdctl join_cluster "$sts_name@$join_pod_name" && sleep 5
+            if [ "$(_cluster_member_count)" = "1" ]
+            then
+                return 3
+            else
+                touch $HOME/.ejabberd_ready && return 0
+            fi
+        elif [ ! "$election_leader" = "$pod_name" ] && [ "$(_cluster_member_count)" -gt "1" ]
         then
             return 0
         fi
